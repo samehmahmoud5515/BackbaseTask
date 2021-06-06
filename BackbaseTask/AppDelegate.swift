57b1d74d385367b949d841d1af0abd593e7a3595
SAMEH <sameh.saad2@vodafone.com>
2021-06-07 01:11:12 +0200

    add base app

---
+++ BackbaseTask/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  BackbaseTask
+//
+//  Created by SAMEH on 05/06/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        
+        return true
+    }
+    
+    
+}
+
