2d1a76aa7d933a74a433f459105c480ac6d7a4c7
SAMEH <sameh.saad2@vodafone.com>
2021-06-07 01:28:57 +0200

    add cities view controller

---
+++ BackbaseTask/Modules/Cities Module/View/ViewController/CitiesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CitiesViewController.swift
+//  BackbaseTask
+//
+//  Created by SAMEH on 07/06/2021.
+//
+
+import UIKit
+
+class CitiesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
