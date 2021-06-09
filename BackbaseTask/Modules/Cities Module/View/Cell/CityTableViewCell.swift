ba68967808e57c32215f0c93906005213276d2f9
SAMEH <sameh.saad2@vodafone.com>
2021-06-09 03:06:20 +0200

    add cities ui

--- BackbaseTask/Modules/Cities Module/View/Cell/CityTableViewCell.swift
+++ BackbaseTask/Modules/Cities Module/View/Cell/CityTableViewCell.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 class CityTableViewCell: UITableViewCell {
+    // MARK: - Outlets
+    @IBOutlet weak var citiyNameLabel: UILabel!
+}
 
-    
+// MARK: - Update UI
+extension CityTableViewCell {
+    func updateUI(with city: City) {
+        let attributedText = NSMutableAttributedString()
+        attributedText.append(NSAttributedString(string: city.name, attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17, weight: .regular)]))
+        attributedText.append(NSAttributedString(string: " (\(city.country))", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17, weight: .bold)]))
+        citiyNameLabel.attributedText = attributedText
+    }    
 }
--- BackbaseTask/Modules/Cities Module/View/ViewController/CitiesViewController.swift
+++ BackbaseTask/Modules/Cities Module/View/ViewController/CitiesViewController.swift
@@ -11,23 +11,15 @@ class CitiesViewController: UIViewController {
     
     // MARK:- Outlets
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     // MARK:- Attribuites
-    private var presnter: CitiesPresenterProtocol!
-    
-    init() {
-        super.init(nibName: "\(CitiesViewController.self)", bundle: nil)
-        presnter = CitiesPresenter(view: self)
-    }
-    
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    var presenter: CitiesPresenterProtocol!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        presenter.fetchCities()
     }
 
 }
@@ -42,16 +34,19 @@ extension CitiesViewController {
     
     private func setupNaviagtionBarUI() {
         navigationController?.setNavigationBarHidden(false, animated: false)
-        navigationController?.navigationBar.barTintColor = .white
-        navigationController?.navigationBar.prefersLargeTitles = false
-        navigationItem.largeTitleDisplayMode = .never
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = CitiesLocalizer.title.rawValue
     }
     
     private func addSearchBarToNaviagtionBar() {
-        let searchBar = UISearchBar()
-        searchBar.barTintColor = .black
-        searchBar.tintColor = .black
-        navigationItem.titleView = searchBar
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.showsCancelButton = false
+        searchController.searchBar.placeholder = CitiesLocalizer.search.rawValue
+        searchController.searchBar.tintColor = .white
+        searchController.searchBar.delegate = self
+        searchController.hidesNavigationBarDuringPresentation = false
+        navigationItem.hidesSearchBarWhenScrolling = false
+        navigationItem.searchController = searchController
     }
     
     private func registerTableViewCell() {
@@ -60,18 +55,38 @@ extension CitiesViewController {
     }
 }
 
+extension CitiesViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        presenter.searchCities(qurey: searchText)
+    }
+}
+
 extension CitiesViewController: CitiesViewControllerProtocol {
+    func reloadData() {
+        tableView.reloadData()
+    }
     
+    func startLoadingIndicator() {
+        activityIndicator.startAnimating()
+    }
+    
+    func stopLoadingIndicator() {
+        activityIndicator.stopAnimating()
+    }
 }
 
 extension CitiesViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return presnter.getCitiesCount()
+        return presenter.getCitiesCount()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "\(CityTableViewCell.self)", for: indexPath)
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "\(CityTableViewCell.self)", for: indexPath) as? CityTableViewCell ?? CityTableViewCell()
+        cell.updateUI(with: presenter.getCity(for: indexPath.row))
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
     }
 }
