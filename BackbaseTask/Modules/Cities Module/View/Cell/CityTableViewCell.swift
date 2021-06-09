3569db4150ab026e955d881e4f344297de7089f2
SAMEH <sameh.saad2@vodafone.com>
2021-06-09 21:01:18 +0200

    add coordinates to city cell

--- BackbaseTask/Modules/Cities Module/View/Cell/CityTableViewCell.swift
+++ BackbaseTask/Modules/Cities Module/View/Cell/CityTableViewCell.swift
@@ -9,15 +9,29 @@ import UIKit
 
 class CityTableViewCell: UITableViewCell {
     // MARK: - Outlets
-    @IBOutlet weak var citiyNameLabel: UILabel!
+    @IBOutlet weak var cityTitleLabel: UILabel!
+    @IBOutlet weak var coordinateLabel: UILabel!
 }
 
 // MARK: - Update UI
 extension CityTableViewCell {
     func updateUI(with city: City) {
+        updateCityTitleLabel(cityName: city.name, countryName: city.country)
+        updateCoordinateLabel(coordinate: city.coord)
+    }
+    
+    private func updateCityTitleLabel(cityName: String, countryName: String) {
         let attributedText = NSMutableAttributedString()
-        attributedText.append(NSAttributedString(string: city.name, attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17, weight: .regular)]))
-        attributedText.append(NSAttributedString(string: " (\(city.country))", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17, weight: .bold)]))
-        citiyNameLabel.attributedText = attributedText
-    }    
+        attributedText.append(NSAttributedString(string: cityName, attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17, weight: .regular)]))
+        attributedText.append(NSAttributedString(string: " (\(countryName))", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17, weight: .bold)]))
+        cityTitleLabel.attributedText = attributedText
+    }
+    
+    private func updateCoordinateLabel(coordinate: City.Coordinate) {
+        let coordinateTitle = CitiesLocalizer.coordinates.rawValue
+        let latitudeTitle = CitiesLocalizer.latitude.rawValue + " \(coordinate.lat)"
+        let longitudeTitle = CitiesLocalizer.longitude.rawValue + " \(coordinate.lon)"
+        let text = "\(coordinateTitle): \n\(latitudeTitle) \n\(longitudeTitle)"
+        coordinateLabel.text = text
+    }
 }
